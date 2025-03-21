b0c5d77d8736ded143d86748a103d17e6fbde0bf
Idzey <kir.pan.787@gmail.com>
2025-03-21 15:48:24 +0300

    add vercel v2

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
-import config from "./utils/config";
 
-const port = config.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+app.get("/", (_req, res) => {
+    res.send("Hello from Express on Vercel!");
 });
+
+module.exports = app;
